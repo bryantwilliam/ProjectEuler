5c34f87855676ce611f9cc5110f1e53bf9371f32
gogobebe2 <gogobebe2@gmail.com>
2015-01-04 20:58:03 +1300

    Starting Project 8

--- src/Page1/LargestProductInASeries.java
+++ src/Page1/LargestProductInASeries.java
@@ -2,7 +2,8 @@ package Page1;
 
 //Problem 8
 public class LargestProductInASeries {
+    public static final long NUMBER = 1231323141231231L;
 	public static void main(String[] args) {
-			
+
 	}
 }
