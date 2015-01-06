c8813b4bd610457568af7a435cc597f45a43ae39
gogobebe2 <gogobebe2@gmail.com>
2015-01-07 08:38:50 +1300

    SpecialPythagoreanTriplet

--- src/Page1/SpecialPythagoreanTriplet.java
+++ src/Page1/SpecialPythagoreanTriplet.java
@@ -5,11 +5,10 @@ public class SpecialPythagoreanTriplet {
     public static void main(String args[]) {
         for (int a = 0; a < 499; a++) {
             for (int b = 0; b < 498; b++) {
-                double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+                double c = Math.sqrt(a * a + b * b);
                 if (((a < b) & (b < c)) & ((a + b + c) == 1000)) {
                     System.out.println("Product: " + a * b * (int) c);
                 }
-
             }
         }
     }
