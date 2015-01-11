d637442cc3d98df7ba8986139243207a8336a8bf
gogobebe2 <gogobebe2@gmail.com>
2015-01-12 08:00:24 +1300

    asdasd

--- src/Page1/SummationOfPrimes.java
+++ src/Page1/SummationOfPrimes.java
@@ -2,24 +2,27 @@ package Page1;
 
 //Problem 10
 public class SummationOfPrimes {
-    public static final int ALL_PRIMES_BELOW = 2_000_000z;
+    public static final int ALL_PRIMES_BELOW = 2_000_000;
 
     public static void main(String args[]) {
         long begTime = System.currentTimeMillis();
         long sumPrimes = 0;
         for (int guess = 2; guess < ALL_PRIMES_BELOW; guess++) {
             boolean testFailed = false;
-                 //Trial and error
-                for (int numbersBelowGuess = 2; numbersBelowGuess < guess; numbersBelowGuess++) {
+            //Trial and error
+            if (guess % 2 == 0 && guess != 2) {
+                testFailed = true;
+            } else {
+                for (int numbersBelowGuess = 3; numbersBelowGuess < Math.sqrt(guess + 1); numbersBelowGuess += 2) {
                     if (guess % numbersBelowGuess == 0) {
                         testFailed = true;
                     }
                 }
+            }
                 /* //Fermat's Little Theorem
                 if (Math.pow(3, guess) % guess != 3 % guess) {
                     testFailed = true;
                 }*/
-
 
 
             if (!testFailed) {
@@ -32,13 +35,15 @@ public class SummationOfPrimes {
         System.out.printf("Total time taken to run this bad boy: " + (endTime - begTime) + " mili-seconds");
     }
 
+
+
 }
 
 /*
 OUTPUT FOR: 2_000_000 as ALL_PRIMES_BELOW value:
 
-The sum of all the primes that are below 2000000 is 142913828922.
-Total time taken to run this bad boy: 9595000 mili-seconds
+The sum of all the primes that are below 2_000_000 is 142913828922.
+Total time taken to run this bad boy: 6671 mili-seconds
 */
 
 
