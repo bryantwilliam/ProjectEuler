02d808cbafefd2349d66ab40a29893cf7868c301
gogobebe2 <gogobebe2@gmail.com>
2016-02-01 21:52:47 +1300

    solved problem 11

--- src/Page1/Problem11.java
+++ src/Page1/Problem11.java
@@ -7,7 +7,7 @@ public class Problem11 {
     private static final char[] NUMBER_CHARS = "08022297381500400075040507785212507791084949994017811857608717409843694804566200814931735579142993714067538830034913366552709523046011426924685601325671370236912231167151676389419236542240402866331380244732609903450244753353783684203517125032988128642367102638406759547066183864706726206802621220956394396308409166499421245558056673992697177878968314883489637221362309750076442045351400613397343133957817532822753167159403800462161409535692163905429635314755588824001754243629855786560048357189070544443744602158515417581980816805944769287392138652177704895540045208839735991607975732162626793327986688366887576220720346336746551232639353690442167338253911249472180846293240627636206936417230238834629969826759857404361620733529783190017431497148868116235705540170547183515469169233486143520189196748"
             .toCharArray();
     private static final List<Integer> PAIRS;
-    private static final int SQUARE_SIDE_LENGTH = 40;
+    private static final int SQUARE_SIDE_LENGTH = 20;
 
     static {
         PAIRS = new ArrayList<>();
@@ -41,32 +41,32 @@ public class Problem11 {
             }
 
             // Up pairs
-            if (!(pairIndex - (40 * 4) < 0)) {
+            if (!(pairIndex - (SQUARE_SIDE_LENGTH * 4) < 0)) {
                 greatestProductSoFar = getGreatestProductSoFar(greatestProductSoFar, getUp4Pairs(pairIndex));
             }
 
             // Down pairs
-            if (!(pairIndex + (40 * 4) > PAIRS.size())) {
+            if (!(pairIndex + (SQUARE_SIDE_LENGTH * 4) > PAIRS.size())) {
                 greatestProductSoFar = getGreatestProductSoFar(greatestProductSoFar, getDown4Pairs(pairIndex));
             }
 
             // Up/Left Diagonal pairs
-            if (!(pairIndex - ((40 + 1) * 4) < 0)) {
+            if (!(pairIndex - ((SQUARE_SIDE_LENGTH + 1) * 4) < 0)) {
                 greatestProductSoFar = getGreatestProductSoFar(greatestProductSoFar, getUpLeftDiagonal4Pairs(pairIndex));
             }
 
             // Up/Right Diagonal pairs
-            if (!(pairIndex - ((40 - 1) * 4) < 0)) {
+            if (!(pairIndex - ((SQUARE_SIDE_LENGTH - 1) * 4) < 0)) {
                 greatestProductSoFar = getGreatestProductSoFar(greatestProductSoFar, getUpRightDiagonal4Pairs(pairIndex));
             }
 
             // Down/Left Diagonal pairs
-            if (!(pairIndex + ((40 - 1) * 4) > PAIRS.size())) {
+            if (!(pairIndex + ((SQUARE_SIDE_LENGTH - 1) * 4) > PAIRS.size())) {
                 greatestProductSoFar = getGreatestProductSoFar(greatestProductSoFar, getDownLeftDiagonal4Pairs(pairIndex));
             }
 
             // Down/Right Diagonal pairs
-            if (!(pairIndex + ((40 + 1) * 4) > PAIRS.size())) {
+            if (!(pairIndex + ((SQUARE_SIDE_LENGTH + 1) * 4) > PAIRS.size())) {
                 greatestProductSoFar = getGreatestProductSoFar(greatestProductSoFar, getDownLeftDiagonal4Pairs(pairIndex));
             }
 
@@ -96,31 +96,40 @@ public class Problem11 {
 
     private static List<Integer> getUp4Pairs(int pairIndex) {
         List<Integer> pairs = new ArrayList<>();
+        for (int index = pairIndex; index > pairIndex - (SQUARE_SIDE_LENGTH * 4); index-=SQUARE_SIDE_LENGTH) pairs.add(PAIRS.get(index));
         return pairs;
     }
 
     private static List<Integer> getDown4Pairs(int pairIndex) {
         List<Integer> pairs = new ArrayList<>();
+        for (int index = pairIndex; index < pairIndex + (SQUARE_SIDE_LENGTH * 4); index+=SQUARE_SIDE_LENGTH) pairs.add(PAIRS.get(index));
         return pairs;
     }
 
     private static List<Integer> getUpLeftDiagonal4Pairs(int pairIndex) {
         List<Integer> pairs = new ArrayList<>();
+        for (int index = pairIndex; index > pairIndex - ((SQUARE_SIDE_LENGTH + 1) * 4); index-=SQUARE_SIDE_LENGTH + 1) pairs.add(PAIRS.get(index));
         return pairs;
     }
 
     private static List<Integer> getUpRightDiagonal4Pairs(int pairIndex) {
         List<Integer> pairs = new ArrayList<>();
-        return pairs;
-    }
-
-    private static List<Integer> getDownRightDiagonal4Pairs(int pairIndex) {
-        List<Integer> pairs = new ArrayList<>();
+        for (int index = pairIndex; index > pairIndex - ((SQUARE_SIDE_LENGTH - 1) * 4); index-=SQUARE_SIDE_LENGTH - 1) pairs.add(PAIRS.get(index));
         return pairs;
     }
 
     private static List<Integer> getDownLeftDiagonal4Pairs(int pairIndex) {
         List<Integer> pairs = new ArrayList<>();
+        for (int index = pairIndex; index < pairIndex + ((SQUARE_SIDE_LENGTH - 1) * 4); index+=SQUARE_SIDE_LENGTH - 1) pairs.add(PAIRS.get(index));
         return pairs;
     }
+
+    private static List<Integer> getDownRightDiagonal4Pairs(int pairIndex) {
+        List<Integer> pairs = new ArrayList<>();
+        for (int index = pairIndex; index < pairIndex + ((SQUARE_SIDE_LENGTH + 1) * 4); index+=SQUARE_SIDE_LENGTH + 1) pairs.add(PAIRS.get(index));
+        return pairs;
+    }
+/*
+    OUTPUT: 70600674
+*/
 }
